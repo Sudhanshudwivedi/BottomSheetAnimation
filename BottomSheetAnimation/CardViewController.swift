279876124052088c10b58da37e8d8c406db4588d
Sudhanshu Dwivedi <sudhanshu004@gmail.com>
2019-08-14 12:51:49 +0530

    First Commit

---
+++ BottomSheetAnimation/CardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CardViewController.swift
+//  BottomSheetAnimation
+//
+//  Created by Sudhanshu Dwivedi on 28/01/19.
+//  Copyright © 2019 Sudhanshu Dwivedi. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet weak var handleArea: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
